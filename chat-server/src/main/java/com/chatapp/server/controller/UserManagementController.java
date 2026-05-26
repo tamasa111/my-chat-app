01721de230aaf5429fabc170beebb6b418e5154a
Tamasa <tamasa111@mail.com>
2026-05-26 20:17:47 +0000

    bugfix

--- chat-server/src/main/java/com/chatapp/server/controller/UserManagementController.java
+++ chat-server/src/main/java/com/chatapp/server/controller/UserManagementController.java
@@ -1,8 +1,11 @@
 package com.chatapp.server.controller;
 
 import com.chatapp.server.service.UserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,15 +38,47 @@ public class UserManagementController {
     }
 
     @PostMapping("/users")
-    public String createUser(@ModelAttribute("form") UserForm form, RedirectAttributes redirectAttributes) {
-        userService.createUser(form.username(), form.password());
-        redirectAttributes.addFlashAttribute("message", "User created: " + form.username());
+    public String createUser(
+            @Valid @ModelAttribute("form") UserForm form,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "user-form";
+        }
+
+        try {
+            userService.createUser(form.getUsername(), form.getPassword());
+        } catch (IllegalArgumentException exception) {
+            bindingResult.reject("createUser", exception.getMessage());
+            return "user-form";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "User created: " + form.getUsername().trim());
         return "redirect:/users";
     }
 
-    public record UserForm(String username, String password) {
-        public UserForm() {
-            this("", "");
+    public static class UserForm {
+        @NotBlank(message = "Username is required")
+        private String username = "";
+
+        @NotBlank(message = "Password is required")
+        private String password = "";
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
         }
     }
 }
